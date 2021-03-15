3580b8274e8d6e84894de879a1b0bbd0c4007062
derpaile <79582032+derpaile@users.noreply.github.com>
2021-03-15 18:21:41 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-hoehe = 500;
+hoehe = 700;
 breite = 500;
-let RHour = 130;
-let RMinute = 200;
+let RHour= 130;
+let RMinute= 200;
 let RSecond = 200;
 let angleHour;
 let angleMinute = 0;
@@ -10,8 +10,6 @@ let angleSecond = 0;
 function setup() {
   createCanvas(breite, hoehe);
   angleMode(DEGREES)
-
-
 }
 
 
@@ -30,83 +28,118 @@ function clockFace() {
     vertex(x, y);
     i++;
   }
+  
   endShape();
   beginShape(POINTS);
-  
   strokeWeight(10);
   stroke(255);
-  var i = 0;
+  i = 0;
+  
   while (i < numMinutes) {
     x = cos(angleMinutes * i) * RSecond;
     y = sin(angleMinutes * i) * RSecond;
     vertex(x, y);
     i++;
   }
-  
-  
-  
-  
   endShape();
 
 }
 
-
-
 function draw() {
   background(0);
-  let Time = getTime();
-  translate(width / 2, height / 2);
+  translate(width/2, height/2);
   clockFace();
-  hourarrow(Time[0]);
-  minutearrow(Time[1]);
-  secondarrow(Time[2]);
-  //console.log(Time);
-
+  Hour = hour();
+  HourSingleDigit = abs(hour()-12)
+  Minute = minute();
+  Second = second();
+  Month = month();
+  Day = day();
+  Year = year();
+  hourarrow();
+  minutearrow();
+  secondarrow();
+  textclock();
+  textdate();
 
 }
 
 
-function getTime() {
-  var Hour = abs(hour() - 12);
-  var Minute = minute();
-  var Second = second();
-  return [Hour, Minute, Second]
-}
 
-
-function hourarrow(Hour) {
-  angleHour = 30 * Hour - 90;
+function hourarrow()  {
+  angleHour = 30*HourSingleDigit-90;
 
   let x1 = 0;
   let y1 = 0;
-  let x2 = cos(angleHour) * RHour;
-  let y2 = sin(angleHour) * RHour;
+  let x2 = cos(angleHour)*RHour;
+  let y2 = sin(angleHour)*RHour;
   stroke(255);
   strokeWeight(7);
 
-  line(x1, y1, x2, y2)
+  line(x1,y1,x2,y2)
 }
 
-function minutearrow(Minute) {
-  angleMinute = 6 * Minute - 90;
+function minutearrow() {
+  angleMinute = 6*Minute-90;
   let x1 = 0;
   let y1 = 0;
-  let x2 = cos(angleMinute) * RMinute;
-  let y2 = sin(angleMinute) * RMinute;
-  stroke(255);
-  strokeWeight(5)
+  let x2 = cos(angleMinute)*RMinute;
+  let y2 = sin(angleMinute)*RMinute;
+  stroke(0,158,224);
+  strokeWeight(5)  
+  
 
-
-  line(x1, y1, x2, y2)
+  line(x1,y1,x2,y2)
 }
 
-function secondarrow(Second) {
-  angleSecond = 6 * Second - 90;
+function secondarrow() {
+  angleSecond = 6*Second-90;
   let x1 = 0;
   let y1 = 0;
-  let x2 = cos(angleSecond) * RSecond;
-  let y2 = sin(angleSecond) * RSecond;
-  stroke(255, 24, 42, 255);
+  let x2 = cos(angleSecond)*RSecond;
+  let y2 = sin(angleSecond)*RSecond;
+  stroke(255,24,42,255);
   strokeWeight(2)
-  line(x1, y1, x2, y2)
+  line(x1,y1,x2,y2)
+}
+
+function textclock() {
+
+  stroke(255);
+  fill(0,158,224);
+  textFont('ComicSansMS',55);  
+  
+  if (Minute<10){
+    stringMinute = "0" + String(Minute);
+  }
+  else{
+    stringMinute= String(Minute);
+  }
+  
+  if (Hour<10){
+    stringHour = "0" + String(Hour);
+    }
+  else{
+    stringHour= String(Hour);
+  }
+  
+  if (Second<10){
+    stringSecond = "0" + String(Second);
+  }
+  else{
+    stringSecond = String(Second);
+  }
+  stringTime = stringHour + ":" + stringMinute + ":" + stringSecond;
+  text(stringTime, -breite/2+140,-hoehe/2+60)
+ 
+}
+
+function textdate(){
+
+stringDate = String(Day) + "." + String(Month) + "." + String(Year);
+stroke(255);
+fill(0,158,224);
+textFont('ComicSansMS',45); 
+text(stringDate, -breite/2+147,+hoehe/2-40);
+
 }
